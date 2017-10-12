8ca40aee1f3999cfe2c244bc9e175e8108acdf6a
aspfit <32666532+aspfit@users.noreply.github.com>
2017-10-12 10:43:09 +0200

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -12,14 +12,6 @@ struct Node {
 	}
 };
 
-struct LinkedList {
-	Node *_head;
-
-	void addFirst(T info) {
-		auto *newNode = new Node(info, _head);
-		_head = newNode;
-	}
-};
 
 int main() {
 	Node *first = new Node(10);
