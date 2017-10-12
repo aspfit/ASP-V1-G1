bea5c7e66f2788256a65067ab2e1bb806497d546
aspfit <32666532+aspfit@users.noreply.github.com>
2017-10-12 11:26:39 +0200

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -7,6 +7,7 @@ public:
 	Node<T> *_head;
 
 	void addFirst(T info);
+	void removeLast();
 };
 
 template<class T>
@@ -14,3 +15,17 @@ void LinkedList<T>::addFirst(T info) {
 	auto *newNode = new Node<T>(info, _head);
 	_head = newNode;
 }
+
+template<class T>
+void LinkedList<T>::removeLast() {
+	Node<T> *temp = this->_head;
+	Node<T> *previousTemp = nullptr;
+	while (temp->_next != nullptr) {
+		previousTemp = tmp;
+		temp = temp->Next;
+	}
+	
+	previousTemp->_next = nullptr;
+	delete temp;
+}
+
