ee02dca12bb62fa65089221befcc533d8e6b8a01
aspfit <32666532+aspfit@users.noreply.github.com>
2017-10-12 11:06:06 +0200

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -1,7 +1,5 @@
 #pragma once
-#include <iostream>
 #include "Node.h";
-using namespace std;
 
 struct LinkedList {
 	Node *_head;
@@ -9,12 +7,5 @@ struct LinkedList {
 	void addFirst(T info) {
 		auto *newNode = new Node(info, _head);
 		_head = newNode;
-	}
-	void print() {
-		Node *tempNode = _head;
-		while (tempNode != nullptr) {
-			cout << tempNode->_info << endl;
-			tempNode = tempNode->_next;
-		}
 	}
 };
