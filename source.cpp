dbac5b0e94a892cda65cd033b7ba6a7a5bc4e579
aspfit <32666532+aspfit@users.noreply.github.com>
2017-10-12 11:03:37 +0200

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -3,15 +3,11 @@
 using namespace std;
 
 int main() {
-	Node *first = new Node(10);
-	Node *second = new Node(9, first);
-	cout << first->_info << endl; //10
-	cout << second->_next->_info << endl; //10
-
 	LinkedList newList;
 	newList.addFirst(5);
 	newList.addFirst(10);
 	newList.addFirst(15);
+	newList.print();
 
 	getchar();
 	return 0;
