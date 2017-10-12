8d1bccad0da4e94f0167dd55672fae52bb20d2ae
aspfit <32666532+aspfit@users.noreply.github.com>
2017-10-12 11:02:16 +0200

    Update Node.h

--- Node.h
+++ Node.h
@@ -5,8 +5,11 @@ struct Node {
 	T _info;
 	Node *_next;
 
-	Node(T info, Node * next = nullptr) {
-		_info = info;
-		_next = next;
-	}
+	Node(T info, Node * next = nullptr);
 };
+
+template <class T>
+Node<T>::Node(T info, Node *next) {
+	_info = info;
+	_next = next;
+}
